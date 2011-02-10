efb8a8fe9cfa7a4990547084233803adee711186
Gonzalo J. Carracedo <BatchDrake@gmail.com>
2011-02-10 15:49:36 +0100

    First actual release

---
+++ libsim.h
@@ -0,0 +1,13 @@
+#ifndef _LIBSIM_H
+#define _LIBSIM_H
+
+#include "common/util.h"
+#include "common/vector.h"
+#include "common/list.h"
+#include "common/hook.h"
+#include "draw/draw.h"
+#include "wbmp/wbmp.h"
+#include "pso/pso.h"
+
+#endif /* _LIBSIM_H */
+
